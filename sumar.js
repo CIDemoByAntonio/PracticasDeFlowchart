6b6c8a7aa80527e2e21c0a46afcb44a6cd6c4aff
Michael <michaelromevi@gmail.com>
2024-05-24 22:25:29 +0200

    Mi primer commit

---
+++ sumar.js
@@ -0,0 +1,19 @@
+//Las lineas que van para arriba son un ciclo
+//for y while
+let num = 3
+for (let x = 1; x <= num; x++) {
+    console.log(x * num)
+}
+//Testigo visual                   
+//x = 1; 1*3 = 3
+//x = 2; 2*3 = 6
+//x = 3; 3*3 = 9
+console.log('------------------------------')
+
+let num2 = 3
+let x = 1 //Contador 
+while (x <= num2) {
+    console.log(x * num2)
+    x++
+}
+
