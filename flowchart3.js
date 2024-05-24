e3bf20e0e852426613edfc26583b205842f5961a
Michael <michaelromevi@gmail.com>
2024-05-24 23:41:46 +0200

    Flowchart3

---
+++ flowchart3.js
@@ -0,0 +1,17 @@
+let sum = 0
+for (let i = 1; i < 10; i++) {
+    sum = sum + i
+    console.log(i)
+}
+console.log(sum)
+console.log('--------------------------')
+
+ sum = 0
+let I = 1
+while (I < 10) { 
+    sum = sum + I 
+    console.log(I) 
+    I++ //2
+}
+console.log(sum)
+
